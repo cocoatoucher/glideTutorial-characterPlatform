ba5d2689e1086ec3e61c8eb7886933f2aa68ea8b
cocoatoucher <cocoatoucher@posteo.se>
2019-05-16 14:40:09 +0200

    Part 3

---
+++ Shared/GameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameViewController.swift
+//  glideTutorialCharacterPlatform
+//
+//  Created by glide on 2019-05-16.
+//
+
+import GlideEngine
+import SpriteKit
+
+class GameViewController: ViewControllerType {
+    
+}
