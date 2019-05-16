7c6c02809b05555613f600c70c4b6b82735c134a
cocoatoucher <cocoatoucher@posteo.se>
2019-05-16 15:52:48 +0200

    Part 9

--- Shared/CharacterEntity.swift
+++ Shared/CharacterEntity.swift
@@ -44,5 +44,61 @@ class CharacterEntity: GlideEntity {
         // `playerIndex` parameter is actually the mapping to input handling.
         let playableCharacterComponent = PlayableCharacterComponent(playerIndex: 0)
         addComponent(playableCharacterComponent)
+        
+        setupTextureAnimations()
+        
+        // Add a custom component for the character for managing texture animations.
+        let characterComponent = CharacterComponent()
+        addComponent(characterComponent)
+    }
+    
+    func setupTextureAnimations() {
+        let timePerFrame: TimeInterval = 0.15
+        let animationSize = CGSize(width: 120, height: 165)
+        let animationOffset = CGPoint(x: 0, y: 15)
+        
+        // Idle animation
+        let idleAction = TextureAnimation.Action(textureFormat: "character_walk_%d",
+                                                 numberOfFrames: 1,
+                                                 timePerFrame: timePerFrame)
+        let idleAnimation = TextureAnimation(triggerName: "Idle",
+                                             offset: animationOffset,
+                                             size: animationSize,
+                                             action: idleAction,
+                                             loops: true)
+        
+        // Add an animator component to our character
+        // with idle animation playing by default
+        let animatorComponent = TextureAnimatorComponent(entryAnimation: idleAnimation)
+        addComponent(animatorComponent)
+        
+        // Walk animation
+        let walkAction = TextureAnimation.Action(textureFormat: "character_walk_%d",
+                                                 numberOfFrames: 3,
+                                                 timePerFrame: timePerFrame)
+        let walkAnimation = TextureAnimation(triggerName: "Walk",
+                                             offset: animationOffset,
+                                             size: animationSize,
+                                             action: walkAction,
+                                             loops: true)
+        animatorComponent.addAnimation(walkAnimation)
+    }
+}
+
+// Custom component for the character entity.
+// Used to control texture animator.
+class CharacterComponent: GKComponent, GlideComponent {
+    
+    func didUpdate(deltaTime seconds: TimeInterval) {
+        
+        // Update texture animator every frame in respect to movement direction.
+        let horizontalMovementComponent = entity?.component(ofType: HorizontalMovementComponent.self)
+        let textureAnimatorComponent = entity?.component(ofType: TextureAnimatorComponent.self)
+        
+        if horizontalMovementComponent?.movementDirection != .stationary {
+            textureAnimatorComponent?.enableAnimation(with: "Walk")
+        } else {
+            textureAnimatorComponent?.enableAnimation(with: "Idle")
+        }
     }
 }
