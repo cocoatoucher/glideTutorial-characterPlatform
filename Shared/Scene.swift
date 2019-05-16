285c2e4412160dbd94a48df6c3e7be3610960dc2
cocoatoucher <cocoatoucher@posteo.se>
2019-05-16 15:20:30 +0200

    Part 5

--- Shared/Scene.swift
+++ Shared/Scene.swift
@@ -16,5 +16,22 @@ class Scene: GlideScene {
         shouldPauseWhenAppIsInBackground = false
         
         backgroundColor = .brown
+        
+        // Add a platform entity in our scene.
+        // Position in roughly in the middle of macOS app window.
+        // Keep in mind that x:0, y:0 is the bottom left corner of the scene in SpriteKit.
+        addEntity(platformEntity(at: CGPoint(x: 512, y: 150)))
+    }
+    
+    func platformEntity(at position: CGPoint) -> GlideEntity {
+        // Create a generic glide entity.
+        let entity = GlideEntity(initialNodePosition: position)
+        
+        // Create a sprite node for our entity to display a texture.
+        let spriteNodeComponent = SpriteNodeComponent(nodeSize: CGSize(width: 640, height: 64))
+        spriteNodeComponent.spriteNode.texture = SKTexture(imageNamed: "platform")
+        entity.addComponent(spriteNodeComponent)
+        
+        return entity
     }
 }
